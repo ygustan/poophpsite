bc09cf4ddf42274ad3877f30dbd965132ca80c3f
Yoann G <y.gustan@ecole-ipssi.net>
2021-07-19 13:04:20 +0200

    Fix rq home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends DefaultController{
         // $prep = $this->db->getPDO()->prepare($statement);
 
         $results = $this->db->getData("SELECT * FROM Produit ORDER BY Id_produit DESC limit 5", "Produit");
-        $animaux = $this->db->getData("SELECT * FROM Animaux ORDER BY Id_animal DESC limit 5", "Animaux");
+        $animaux = $this->db->getData("SELECT * FROM Animaux WHERE Adopter = False ORDER BY Id_animal DESC limit 5", "Animaux");
 
         $this->render("base", [
             "results" => $results,
