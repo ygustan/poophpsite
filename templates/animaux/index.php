d0bb383d5add6d49b3b9f0baae720647dafd53ac
Yoann G <y.gustan@ecole-ipssi.net>
2021-07-19 12:39:08 +0200

    Fix l'index

--- templates/animaux/index.php
+++ templates/animaux/index.php
@@ -2,6 +2,6 @@
 
 <ul>
     <?php foreach( $animaux as $animal):?>
-        <li><a href="index.php?method=getAnimal&id=<?= $animal->getId() ?>"><?= $animal->getNomAnimal() ?></a></li>
+        <li><a href="index.php?method=getAnimal&id=<?= $animal->getId_animal() ?>"><?= $animal->getNomAnimal() ?></a></li>
     <?php endforeach ?>
 </ul>
